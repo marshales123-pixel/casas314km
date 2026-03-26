2f9a2dd3deffb769422d83767b04ffb56379b3af
gonzalo <marshales123@gmail.com>
2026-03-26 00:55:58 -0300

    Agrego protección admin

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const url = request.nextUrl;
+
+  if (url.pathname.startsWith("/admin")) {
+    const auth = request.headers.get("authorization");
+
+    if (!auth) {
+      return new NextResponse("Auth required", {
+        status: 401,
+        headers: {
+          "WWW-Authenticate": 'Basic realm="Secure Area"',
+        },
+      });
+    }
+
+    const base64 = auth.split(" ")[1];
+    const decoded = Buffer.from(base64, "base64").toString("utf-8");
+    const [user, password] = decoded.split(":");
+
+    if (
+      user !== process.env.ADMIN_USER ||
+      password !== process.env.ADMIN_PASSWORD
+    ) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+  }
+
+  return NextResponse.next();
+}
